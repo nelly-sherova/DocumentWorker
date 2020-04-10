ce4390113d1e23b24761f20bd26718b20a107bda
unknown <lvova.nikol@mail.ru>
2020-04-10 19:38:07 +0300

    Работа в Main

--- Program.cs
+++ Program.cs
@@ -42,7 +42,30 @@ namespace Document
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string proKey = "pro";
+            const string expKey = "exp";
+            Console.WriteLine("Введите лицензионный ключ!");
+            string key = Console.ReadLine();
+            DocumentWorker document = new DocumentWorker();
+            switch (key)
+            {
+                case proKey: document = new ProDocumentWorker(); break;
+                case expKey: document = new ExpertDocumentWorker(); break;
+                default: document = new DocumentWorker(); break;
+            }
+            Console.WriteLine("Введите команды для работы с документом: \n1-Открыть документ, \n2-Редактировать документ, \n3-Сохранить, \n4-выход");
+            bool b = true;
+            while (b == true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":  Console.ForegroundColor = ConsoleColor.Green; document.OpenDocument(); Console.ForegroundColor = ConsoleColor.White; break;
+                    case "2": Console.ForegroundColor = ConsoleColor.Green; document.EditDocument(); Console.ForegroundColor = ConsoleColor.White; break;
+                    case "3": Console.ForegroundColor = ConsoleColor.Green; document.SaveDocument(); Console.ForegroundColor = ConsoleColor.White; break;
+                    case "4": b =false; Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Выход"); Console.ForegroundColor = ConsoleColor.White; break;
+                    default :Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Неправильная команда!"); Console.ForegroundColor = ConsoleColor.White; break;
+                }
+            }
         }
     }
 }
