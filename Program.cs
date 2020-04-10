c2e6bcebfd0f5db5e3de06a0047b867d024f3d69
unknown <lvova.nikol@mail.ru>
2020-04-10 19:16:04 +0300

    Объявление производного  класса ExpertDocumentWorker и переопределение методов

--- Program.cs
+++ Program.cs
@@ -31,7 +31,13 @@ namespace Document
             Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
         }
     }
-    
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в новом формате");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
