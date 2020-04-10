c4b2ebc2794c08d9ee3e38044d4b10e915707643
unknown <lvova.nikol@mail.ru>
2020-04-10 19:10:16 +0300

    Объявление класса DocumentWorker и его методов

--- Program.cs
+++ Program.cs
@@ -2,6 +2,23 @@
 
 namespace Document
 {
+    class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+ 
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+ 
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
